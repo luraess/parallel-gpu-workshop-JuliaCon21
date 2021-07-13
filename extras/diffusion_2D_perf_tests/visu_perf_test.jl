19556746c5cc37b2f1303ce23c58c62c86ddd350
Ludovic Räss <ludovic.rass@gmail.com>
2021-07-13 10:53:11 +0200

    update perftest material

--- extras/diffusion_2D_perf_tests/visu_perf_test.jl
+++ extras/diffusion_2D_perf_tests/visu_perf_test.jl
@@ -1,4 +1,4 @@
-using DelimitedFiles, Plots
+using DelimitedFiles, Plots, Printf
 
 perf_i5 = readdlm("out_diffusion_2D_damp_perf_i5.txt")
 perf_loop_i5 = readdlm("out_diffusion_2D_damp_perf_loop_i5.txt")
@@ -8,24 +8,31 @@ perf_xpu_v100 = readdlm("out_diffusion_2D_damp_perf_xpu_v100.txt")
 xpu_v100 = readdlm("out_diffusion_2D_damp_xpu_v100.txt")
 
 plot_fig = 1
-save_fig = true
+save_fig = false
 
-fontsize = 12
+fontsize = 11
     opts = (yaxis=font(fontsize, "Courier"), xaxis=font(fontsize, "Courier"),
-            framestyle=:box, titlefontsize=fontsize, titlefont="Courier",
-            linewidth=:3.0, foreground_color_legend = nothing, legendfontsize=11, legend=:topright, 
-            xlims=(-50,4200))
+            framestyle=:box, titlefontsize=fontsize, titlefont="Courier", ylabel="T_eff [GB/s]",
+            linewidth=:3.0, foreground_color_legend = nothing, legendfontsize=11, legend=:topright,
+            size=(600,400) )
 
 if plot_fig==1
 
-p1 = plot(perf_i5[:,1], perf_i5[:,4]; label="CPU broadcast", ylabel="T_eff [GB/s]", ylims=(-1, 32), opts...)
+p1 = plot(perf_i5[:,1], perf_i5[:,4]; xlims=(-50,4200), ylims=(-1, 32), label="CPU broadcast", opts...)
 plot!(perf_loop_i5[:,1], perf_loop_i5[:,4]; label="CPU loop", opts...)
 display(plot!(perf_loop_fun_i5[:,1], perf_loop_fun_i5[:,4]; label="CPU loop function", opts...))
 if save_fig savefig("perf_cpu.png") end
+
 else
 
-p2 = plot(perf_gpu_v100[:,1], perf_gpu_v100[:,4]; label="GPU", xlabel="grid resolution (nx)", ylabel="T_eff [GB/s]", ylims=(-10, 1300), opts...)
+x = perf_gpu_v100[[1,6,7,8,9],1]
+ticks = collect(x)
+ticklabels = [ @sprintf("%.f",x) for x in ticks ]
+
+p2 = plot(perf_gpu_v100[:,1], perf_gpu_v100[:,4]; xlims=(-50, 17000), ylims=(-10, 1300), label="GPU", xlabel="grid resolution (nx)", opts...)
 plot!(perf_xpu_v100[:,1], perf_xpu_v100[:,4]; label="XPU parallel_indices", opts...)
+plot!(xticks=(ticks,ticklabels),xrotation=0)
 display(plot!(xpu_v100[:,1], xpu_v100[:,4]; label="XPU parallel", opts...))
 if save_fig savefig("perf_gpu.png") end
+
 end
