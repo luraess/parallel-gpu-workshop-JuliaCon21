a1832ae8f95670252d8a0807f678c3756b46dfc3
Mauro Werder <mauro3@runbox.com>
2021-07-19 15:49:12 +0200

    DiffEq example

--- scripts/diffusion_2D_DiffEq_expl.jl
+++ scripts/diffusion_2D_DiffEq_expl.jl
@@ -1,4 +1,5 @@
-using Plots, Printf, LinearAlgebra
+# Solve 2D nonlinear diffusion using OrdinaryDiffEq.jl (from the DiffEq.jl / SciML.ai universe)
+using Plots, Printf, LinearAlgebra, OrdinaryDiffEq
 
 # enable plotting by default
 if !@isdefined do_visu; do_visu = true end
@@ -8,7 +9,19 @@ if !@isdefined do_visu; do_visu = true end
 @views av_yi(A) = 0.5*(A[2:end-1,1:end-1].+A[2:end-1,2:end]) # average in y-direction
 @views   inn(A) = A[2:end-1,2:end-1] # computational domain
 
-@views function diffusion_2D_expl(; do_visu=true, save_fig=false)
+function diffusion_2D_obj!(du, u, p, t)
+    H = u
+    dHdt = du
+    npow, dx, dy = p.npow, p.dx, p.dy
+    # TODO: make this allocation-free
+    qHx    = -av_xi(H).^npow.*diff(H[:,2:end-1], dims=1)/dx  # flux
+    qHy    = -av_yi(H).^npow.*diff(H[2:end-1,:], dims=2)/dy  # flux
+    dHdt[2:end-1,2:end-1]  .= -diff(qHx, dims=1)/dx .- diff(qHy, dims=2)/dy     # rate of change
+    dHdt[1:end,1] .= 0; dHdt[1:end,end] .= 0; dHdt[1,1:end] .= 0; dHdt[end,1:end] .= 0 # sets the BC as H[1]=H[end]=0
+    return nothing
+end
+
+function diffusion_2D_DiffEq_expl(;Solver=Tsit5, do_visu=true, save_fig=false)
     # Physics
     lx, ly = 10.0, 10.0   # domain size
     npow   = 3            # power-law exponent
@@ -18,33 +31,22 @@ if !@isdefined do_visu; do_visu = true end
     # Derived numerics
     dx, dy = lx/nx, ly/ny # grid size
     xc, yc = LinRange(dx/2, lx-dx/2, nx), LinRange(dy/2, ly-dy/2, ny)
-    # Array allocation
-    qHx    = zeros(nx-1, ny-2) # on staggered grid
-    qHy    = zeros(nx-2, ny-1) # on staggered grid
-    dHdt   = zeros(nx-2, ny-2) # normal grid, without boundary points
     # Initial condition
-    H      = exp.(.-(xc.-lx/2).^2 .-(yc.-ly/2)'.^2)
-    dt     = minimum(min(dx, dy)^2 ./inn(H).^npow./4.1)  # time step (obeys ~CFL condition)
-    t = 0.0; it = 0
-    # Physical time loop
-    while t<ttot
-        qHx    .= -av_xi(H).^npow.*diff(H[:,2:end-1], dims=1)/dx  # flux
-        qHy    .= -av_yi(H).^npow.*diff(H[2:end-1,:], dims=2)/dy  # flux
-        dHdt   .= -diff(qHx, dims=1)/dx -diff(qHy, dims=2)/dy     # rate of change
-        H[2:end-1,2:end-1] .= inn(H) .+ dt.*dHdt      # update rule, sets the BC as H[1]=H[end]=0
-        t += dt; it += 1
-    end
-    @printf("Total time = %1.2f, time steps = %d \n", round(ttot, sigdigits=2), it)
+    H0      = exp.(.-(xc.-lx/2).^2 .-(yc.-ly/2)'.^2)
+
+    prob = ODEProblem(diffusion_2D_obj!, H0, (0.0, ttot), (npow=npow, dx=dx, dy=dy))
+    @time sol = solve(prob, Solver())
+
     # Visualize
     if do_visu
         fontsize = 12
         opts = (aspect_ratio=1, yaxis=font(fontsize, "Courier"), xaxis=font(fontsize, "Courier"),
                 ticks=nothing, framestyle=:box, titlefontsize=fontsize, titlefont="Courier", colorbar_title="",
                 xlabel="Lx", ylabel="Ly", xlims=(xc[1],xc[end]), ylims=(yc[1],yc[end]), clims=(0.,1.))
-        display(heatmap(xc, yc, H'; c=:davos, title="explicit diffusion (nt=$it)", opts...))
+        display(heatmap(xc, yc, sol.u[end]'; c=:davos, title="DiffEq with solver $Solver", opts...))
         if save_fig savefig("diff2D_expl.png") end
     end
-    return xc, yc, H
+    return xc, yc, sol.u[end]
 end
 
-diffusion_2D_expl(; do_visu=do_visu);
+sol = diffusion_2D_DiffEq_expl(; do_visu=do_visu);
